9c998193e165bacc27b807a6743a9766062dc9e6
Saber Kia <saber.kia.s@gmail.com>
2020-12-27 16:19:16 +0330

    adding database making code + .gitignore for ignoring data folder

---
+++ database_making.m
@@ -0,0 +1,34 @@
+clc
+clear
+close all
+
+Tw    = 25;            % analysis frame duration (ms)
+Ts    = 12.5;          % analysis frame shift (ms)
+alpha = 0.97;          % preemphasis coefficient
+R     = [ 300 8000 ];  % frequency range to consider
+M     = 20;            % number of filterbank channels
+C     = 33;            % number of cepstral coefficients
+L     = 22;            % cepstral sine lifter parameter
+point = 600000;
+sample_rate = 5;       % Downsample Rate
+
+[data,Fs] = audioread('Voice 003.m4a');
+
+for i = 1:
+signal    = data(point:point + 80000)';
+% Downsampling
+signal    = signal(1:sample_rate:end);
+Fs        = Fs/sample_rate;
+plot(signal)
+grid on
+
+% hamming window
+hamming = @(N)(0.54-0.46*cos(2*pi*(0:N-1).'/(N-1)));
+
+% Feature extraction (feature vectors as columns)
+[MFCCs, MFSCs, frames] = ...
+    mfcc_m( signal, Fs, Tw, Ts, alpha, hamming, R, M, C, L );
+
+
+
+
