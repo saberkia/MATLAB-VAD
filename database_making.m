4e03a7bfd1ffd9c9d65628bf4a9f5b270034c85e
Saber Kia <saber.kia.s@gmail.com>
2020-12-28 01:07:39 +0330

    building database of images on HDD with extracted features

--- database_making.m
+++ database_making.m
@@ -9,20 +9,21 @@ R     = [ 300 8000 ];  % frequency range to consider
 M     = 20;            % number of filterbank channels
 C     = 33;            % number of cepstral coefficients
 L     = 22;            % cepstral sine lifter parameter
-WL    = 5000;          % Window Lenght
+WL    = 50000;          % Window Lenght
 %%
-sample_rate = 5;                                       % Downsample Rate
+sample_rate = 3;                                       % Downsample Rate
 [data,Fs] = audioread('Voice 003.m4a');
 data_dspl = data(1:sample_rate:end);                   % Downsampling
 Fs        = Fs/sample_rate;
 hamming   = @(N)(0.54-0.46*cos(2*pi*(0:N-1).'/(N-1))); % hamming window
 %%
-for index = 1:50
+for index = 1:26
     point  = (index -1)*WL + 1;
     signal = data(point:point + WL)';
     % Feature extraction (feature vectors as columns)
     [MFCCs, MFSCs, frames] = ...
         mfcc_m( signal, Fs, Tw, Ts, alpha, hamming, R, M, C, L );
+    feature{index} = MFSCs;
     x(index) = mean(MFSCs(:));
     figure(1)
     imagesc(1:size(MFSCs,2), 0:C-1, MFSCs);
@@ -31,5 +32,6 @@ for index = 1:50
 end
 
 figure
-subplot(2,1,1), plot(x), grid on
+subplot(2,1,1), bar(x), grid on
 subplot(2,1,2), plot(data_dspl), grid on ,axis tight
+save feature feature
